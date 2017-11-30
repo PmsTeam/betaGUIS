46334234889b2f48408020c03ed997149281c73e
shun <814464214@qq.com>
2017-11-30 19:06:05 +0800

    first beta guis

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include "mainwindow.h"
+#include <QApplication>
+#include <QFile>
+class CommonHelper
+{
+public:
+    static void setStyle(const QString &style)
+    {
+        QFile qss(style);
+        qss.open(QFile::ReadOnly);
+        qApp->setStyleSheet(qss.readAll());
+        qss.close();
+    }
+};
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+//    CommonHelper::setStyle("G:/study/QT/betaEyes/style.qss");//设置style
+
+    MainWindow w;
+    w.show();
+
+    return a.exec();
+}
